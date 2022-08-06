e75e911960cc0e3eaa987b8c35df8624c0591b8d
Aaron <schultzaaron@web.de>
2022-08-06 22:09:41 +0200

    ExitLag TcpPacket test

--- src/StatisticsAnalysisTool/Network/NetworkManager.cs
+++ src/StatisticsAnalysisTool/Network/NetworkManager.cs
@@ -7,9 +7,11 @@ using StatisticsAnalysisTool.Network.Manager;
 using StatisticsAnalysisTool.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Windows;
+using Protocol16.Photon;
 
 namespace StatisticsAnalysisTool.Network
 {
@@ -53,6 +55,7 @@ namespace StatisticsAnalysisTool.Network
             {
                 foreach (var device in CapturedDevices)
                 {
+                    Debug.Print(device.Name);
                     PacketEvent(device);
                 }
             }
@@ -85,7 +88,7 @@ namespace StatisticsAnalysisTool.Network
             {
                 device.Open(new DeviceConfiguration()
                 {
-                    Mode = DeviceModes.DataTransferUdp,
+                    Mode = DeviceModes.Promiscuous,
                     ReadTimeout = 5000
                 });
                 device.OnPacketArrival += Device_OnPacketArrival;
@@ -97,10 +100,17 @@ namespace StatisticsAnalysisTool.Network
         {
             try
             {
-                var packet = Packet.ParsePacket(e.GetPacket().LinkLayerType, e.GetPacket().Data).Extract<UdpPacket>();
-                if (packet != null && (packet.SourcePort == 5056 || packet.DestinationPort == 5056))
+                var udpPacket = Packet.ParsePacket(e.GetPacket().LinkLayerType, e.GetPacket().Data).Extract<UdpPacket>();
+                var tcpPacket = Packet.ParsePacket(e.GetPacket().LinkLayerType, e.GetPacket().Data).Extract<TcpPacket>();
+
+                if (udpPacket != null && (udpPacket.SourcePort == 5056 || udpPacket.DestinationPort == 5056))
                 {
-                    _receiver.ReceivePacket(packet.PayloadData);
+                    _receiver.ReceivePacket(udpPacket.PayloadData);
+                }
+
+                if (tcpPacket != null && (tcpPacket.SourcePort == 60598 || tcpPacket.DestinationPort == 6463))
+                {
+                    _receiver.ReceivePacket(tcpPacket.PayloadData);
                 }
             }
             catch (InvalidOperationException ioe)
@@ -118,4 +128,65 @@ namespace StatisticsAnalysisTool.Network
             }
         }
     }
+
+    public class ExitLagParser
+    {
+        private readonly uint[] _raw = new uint[4];
+
+        public uint Version() 
+        {
+            return _raw[0];
+        }
+
+        public void SetVersion(ushort value)
+        {
+            _raw[0] = value;
+        }
+
+        public ulong BodySize()
+        {
+            return StaticCast(_raw[1]) + (StaticCast(_raw[2]) << 8) + (StaticCast(_raw[3]) << 16);
+        }
+
+        public static T StaticCast<T>(T o) => o;
+
+        public void SetBodySize(uint value)
+        {
+            _raw[1] = (value & 0xFF);
+            _raw[2] = ((value >> 8) & 0xFF);
+            _raw[3] = ((value >> 16) & 0xFF);
+        }
+    }
 }
+
+
+
+//EXITLAG_CORE_PACKED_STRUCT_BEGIN(PacketHeader) {
+//    std::array < uint8_t, 4 > raw;
+//    uint8_t version() const noexcept{
+//        return raw[0];
+//    }
+//    void set_version(uint8_t value) noexcept {
+//        raw[0] = value;
+//    }
+//    uint32_t body_size() const noexcept {
+//        return static_cast<uint32_t>(raw[1])
+//               + (static_cast<uint32_t>(raw[2]) << 8) +
+//               +(static_cast<uint32_t>(raw[3]) << 16);
+//    }
+//    void set_body_size(uint32_t value)
+//    {
+//        raw[1] = value & 0xFF;
+//        raw[2] = (value >> 8) & 0xFF;
+//        raw[3] = (value >> 16) & 0xFF;
+//    }
+//}
+
+//EXITLAG_CORE_PACKED_STRUCT_END;
+//2.Struct of a field:
+////  1. Attributes (1 byte):
+////   [0..1] - size of field ID, in bytes
+////   [2..3] - size of field "size", in bytes
+////  2. ID (1-3 bytes)
+////  3. [optional] Remainig data size (1-3 bytes)
+////  4. [optional] Payload (any size)

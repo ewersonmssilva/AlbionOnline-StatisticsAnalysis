ca0ee6b63ba3c1c4d002fb58593bb3dea8356531
Aaron <schultzaaron@web.de>
2022-08-22 15:36:31 +0200

    Added app and user settings

---
+++ src/StatisticsAnalysisTool.Avalonia/Settings/ApplicationSettings.cs
@@ -0,0 +1,41 @@
+﻿namespace StatisticsAnalysisTool.Avalonia.Settings;
+
+public static class ApplicationSettings
+{
+    public static string LanguageDirectoryName => "Languages";
+    public static string ImageResources => "ImageResources";
+    public static string ItemListFileName => "ItemList.json";
+    public static string DonateUrl => "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=LEM458PZ4FV4C&source=url";
+    public static string DefaultItemListSourceUrl => "https://raw.githubusercontent.com/broderickhyman/ao-bin-dumps/master/formatted/items.json";
+    public static string AutoUpdateConfigUrl => "https://raw.githubusercontent.com/Triky313/AlbionOnline-StatisticsAnalysis/main/src/StatisticsAnalysisTool/ao-update-check.xml";
+    public static string DefaultLanguageCultureName => "en-US";
+    public static bool UpgradeRequired => true;
+    public static string PlayerModeUpdateFileName => "PlayerModeInformation.txt";
+    public static string GitHubRepoUrl => "https://github.com/Triky313/AlbionOnline-StatisticsAnalysis";
+    public static string SoundDirectoryName => "Sounds";
+    public static string ActiveAlertsFileName => "ActiveAlerts.json";
+    public static string WorldDataFileName => "world.json";
+    public static string DungeonRunsFileName => "DungeonRuns.json";
+    public static string LootChestDataFileName => "lootchests.json";
+    public static string DiscordUrl => "https://discord.com/invite/sahSrSPmaJ";
+    public static string FavoriteItemsFileName => "FavoriteItems.json";
+    public static string CityPricesApiUrlDefault => "https://www.albion-online-data.com/api/v2/stats/prices/";
+    public static string CityPricesHistoryApiUrlDefault => "https://www.albion-online-data.com/api/v2/stats/history/";
+    public static string GoldStatsApiUrlDefault => "https://www.albion-online-data.com/api/v2/stats/Gold";
+    public static string GameFilesDirectoryName => "GameFiles";
+    public static string SettingsFileName => "Settings.json";
+    public static string ItemSpriteToJournalFileName => "ItemSpriteToJournal.json";
+    public static string DefaultItemsJsonSourceUrl => "https://raw.githubusercontent.com/broderickhyman/ao-bin-dumps/master/items.json";
+    public static string ItemsJsonFileName => "Items.json";
+    public static string AutoUpdatePreReleaseConfigUrl => "https://raw.githubusercontent.com/Triky313/AlbionOnline-StatisticsAnalysis/main/src/StatisticsAnalysisTool/ao-pre-release-update-check.xml";
+    public static string StatsFileName => "Stats.json";
+    public static int KeepDashboardStatisticsForDays => -90;
+    public static string MailsFileName => "Mails.json";
+    public static string VaultsFileName => "Vaults.json";
+    public static string PatreonUrl => "https://www.patreon.com/triky313";
+    public static string CraftingNotesFileName => "CraftingNotes.json";
+    public static string PlayerKillsDeathsFileName => "PlayerKillsDeaths.json";
+    public static string TreasureStatsFileName => "TreasureStats.json";
+    public static string DamageMeterSnapshotsFileName => "DamageMeterSnapshots.json";
+    public static string DonationsUrl => "https://raw.githubusercontent.com/Triky313/AlbionOnline-StatisticsAnalysis/main/donations.json";
+}
---
+++ src/StatisticsAnalysisTool.Avalonia/Settings/SettingsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using Avalonia.Controls;
+using log4net;
+using StatisticsAnalysisTool.Avalonia.Common;
+
+namespace StatisticsAnalysisTool.Avalonia.Settings;
+
+public static class SettingsController
+{
+    public static UserSettings CurrentUserSettings = new();
+
+    private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod()?.DeclaringType);
+    private static bool _haveSettingsAlreadyBeenLoaded;
+
+    public static void SaveSettings(WindowState windowState, double height, double width)
+    {
+        #region Window
+
+        if (windowState != WindowState.Maximized)
+        {
+            CurrentUserSettings.MainWindowHeight = double.IsNegativeInfinity(height) || double.IsPositiveInfinity(height) ? 0 : height;
+            CurrentUserSettings.MainWindowWidth = double.IsNegativeInfinity(width) || double.IsPositiveInfinity(width) ? 0 : width;
+        }
+
+        CurrentUserSettings.MainWindowMaximized = windowState == WindowState.Maximized;
+
+        #endregion
+
+        SaveToLocalFile();
+
+        ItemController.SaveFavoriteItemsToLocalFile();
+    }
+
+    public static void LoadSettings()
+    {
+        if (_haveSettingsAlreadyBeenLoaded)
+        {
+            return;
+        }
+
+        var localFilePath = $"{AppDomain.CurrentDomain.BaseDirectory}{ApplicationSettings.SettingsFileName}";
+
+        if (File.Exists(localFilePath))
+        {
+            try
+            {
+                var settingsString = File.ReadAllText(localFilePath, Encoding.UTF8);
+                CurrentUserSettings = JsonSerializer.Deserialize<UserSettings>(settingsString) ?? throw new ArgumentNullException();
+                _haveSettingsAlreadyBeenLoaded = true;
+            }
+            catch (Exception e)
+            {
+                ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod()?.DeclaringType, e);
+                Log.Error(MethodBase.GetCurrentMethod()?.DeclaringType, e);
+            }
+        }
+    }
+
+    private static void SaveToLocalFile()
+    {
+        var localFilePath = $"{AppDomain.CurrentDomain.BaseDirectory}{ApplicationSettings.SettingsFileName}";
+
+        try
+        {
+            var fileString = JsonSerializer.Serialize(CurrentUserSettings);
+            File.WriteAllText(localFilePath, fileString, Encoding.UTF8);
+        }
+        catch (Exception e)
+        {
+            ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod()?.DeclaringType, e);
+            Log.Error(MethodBase.GetCurrentMethod()?.DeclaringType, e);
+        }
+    }
+}
